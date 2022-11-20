50cb9125a074a546240f501d98c5b2e0b5769277
NeivaYang <yangaraujo.neiva@gmail.com>
2022-11-20 19:01:49 -0300

    One More Adjust commit

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,13 +1,19 @@
 interface ChildProps {
     color: string;
+    onClick: () => void;
+    children?: React.ReactNode;
 }
 
-export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>
-};
-
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+export const Child = ({ color, onClick }: ChildProps) => {
     return <div>
         {color}
+        <button onClick={onClick}>Click me!</button>
+    </div>
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+    return <div>
+        {color}
+        <button onClick={onClick}>Click me!</button>
     </div>
 };
