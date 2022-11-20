a060d2d9d6cd17af00a5b9c1563339f3839233ff
NeivaYang <yangaraujo.neiva@gmail.com>
2022-11-20 18:41:07 -0300

    child commit

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,9 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
     return <div>{color}</div>
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+    return <div>
+        {color}
+    </div>
+};
