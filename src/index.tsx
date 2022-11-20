66d7373150088b3b2af160b91d6b513d9c9cd1c1
NeivaYang <yangaraujo.neiva@gmail.com>
2022-11-20 18:11:13 -0300

    index adjust

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    );
+};
+
+root.render(<App />);
